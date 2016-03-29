cab9f3f53b93510f1a99f4b41ddaed74856cfce5
Daniel Tjuatja <daniel.tjuatja@gmail.com>
2016-03-29 13:33:29 +0800

    - Carthage integration test -

---
+++ Example/DTDropDownListView/DTDropDownListView.h
@@ -0,0 +1,19 @@
+//
+//  DTDropDownListView.h
+//  DTDropDownListView
+//
+//  Created by Daniel Tjuatja on 29/3/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTDropDownListView.
+FOUNDATION_EXPORT double DTDropDownListViewVersionNumber;
+
+//! Project version string for DTDropDownListView.
+FOUNDATION_EXPORT const unsigned char DTDropDownListViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTDropDownListView/PublicHeader.h>
+
+
